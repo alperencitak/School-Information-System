eba9d2479e2dc17a7449b4b452de3a2f1fb13838
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2024-12-14 02:46:14 +0300

    Instructor Login UI Design was made

--- client/app/src/main/java/com/alperencitak/sis/MainActivity.kt
+++ client/app/src/main/java/com/alperencitak/sis/MainActivity.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import com.alperencitak.sis.ui.theme.SisTheme
 import com.alperencitak.sis.view.LoginInstructorScreen
 
@@ -13,6 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SisTheme {
+
                 LoginInstructorScreen()
             }
         }
--- client/app/src/main/java/com/alperencitak/sis/ui/theme/Theme.kt
+++ client/app/src/main/java/com/alperencitak/sis/ui/theme/Theme.kt
@@ -2,15 +2,22 @@ package com.alperencitak.sis.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import com.alperencitak.sis.R
 
 private val DarkColorScheme = darkColorScheme(
     primary = BlueGray,
